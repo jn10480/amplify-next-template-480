8ebe5046bb0b9b0da930d7f93e63a12c822a8542
Luna Ueno <runa.ueno@jnovel.co.jp>
2025-05-27 15:52:49 +0900

    pageに認証機能をまとめる

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
+
+import { useAuthenticator } from "@aws-amplify/ui-react";
+
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
@@ -14,6 +18,8 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  const { signOut } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -48,16 +54,19 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>トークテーマ</h1>
-      <button onClick={pickRandomTodo}>トークテーマを選ぶ!</button>
-      <ul>
-        {todos.map((todo) => (
-          <li onClick={() => deleteTodo(todo)} key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>トークテーマをクリックして削除</div>
-      <button onClick={createTodo}>追加</button>
-    </main>
+    <Authenticator>
+      <main>
+        <h1>トークテーマ</h1>
+        <button onClick={pickRandomTodo}>トークテーマを選ぶ!</button>
+        <ul>
+          {todos.map((todo) => (
+            <li onClick={() => deleteTodo(todo)} key={todo.id}>{todo.content}</li>
+          ))}
+        </ul>
+        <div>トークテーマをクリックして削除</div>
+        <button onClick={createTodo}>追加</button>
+      </main>
+      <button onClick={signOut}>Sign out</button>
+    </Authenticator>
   );
 }
